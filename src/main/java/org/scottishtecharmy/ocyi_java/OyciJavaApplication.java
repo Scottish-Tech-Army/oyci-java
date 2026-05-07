c07e7971322eac85ab084a1cdabde4dd9e6ed601
Fosker <stuart.fosker@lloydsbanking.com>
2026-05-07 07:44:18 +0100

    Initial commit

---
+++ src/main/java/org/scottishtecharmy/ocyi_java/OyciJavaApplication.java
@@ -0,0 +1,13 @@
+package org.scottishtecharmy.ocyi_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OyciJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OyciJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/scottishtecharmy/ocyi_java/OyciJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scottishtecharmy.ocyi_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OyciJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
